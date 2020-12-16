b76adfdeb778963057ba43cbe10041907415d302
WeijieFu <weijiefu92@gmail.com>
2020-12-16 22:34:22 +0100

    add nav triangle

--- components/Home/Nav.vue
+++ components/Home/Nav.vue
@@ -1,25 +1,25 @@
 <template>
     <div class="nav">
-        <div class="nav_about">
-            <svg height="0.5rem" width="0.5rem" class="nav_circle" fill="yellow">
-                 <polygon points="0rem,0.5rem 5rem,0.5rem 0.5rem,0"  />
+        <div class="nav_">
+            <svg height="10" width="10" class="nav_circle">
+                 <polygon points="0,10 10,10 10,0"  />
             </svg>
             
-            <a class="nav_link">ABOUT</a>
+            <a class="nav_link" href="#" v-scroll-to="'#about'">ABOUT</a>
         </div>
-         <div class="nav_work">
-            <svg height="0.5rem" width="0.5rem" class="nav_circle">
-                 <circle cx="0.25rem" cy="0.25rem" r="0.25rem" />
+         <div class="nav_">
+            <svg height="10" width="10" class="nav_circle">
+                 <polygon points="0,10 10,10 10,0"  />
             </svg>
             
-            <a class="nav_link">WORK</a>
+            <a class="nav_link" href="#" v-scroll-to="'#work'">WORK</a>
         </div>
-         <div class="nav_contact">
-            <svg height="0.5rem" width="0.5rem" class="nav_circle">
-                 <circle cx="0.25rem" cy="0.25rem" r="0.25rem" />
+         <div class="nav_">
+            <svg height="10" width="10" class="nav_circle">
+                 <polygon points="0,10 10,10 10,0"  />
             </svg>
             
-            <a class="nav_link">CONTACT</a>
+            <a class="nav_link" href="#" v-scroll-to="'#contact'">CONTACT</a>
         </div>
     </div>
 </template>
@@ -34,14 +34,27 @@ export default {
         width: 90vw;
         position: fixed;
         bottom: 1rem;
-        color: var(--color-text-grey);
+        
          display: flex;
         justify-content: space-between;
         align-items: center;
     }
     .nav_circle{
         fill: #fff;
+        transition: all 0.2s ease-out;
     }
-   
+    .nav_:hover > .nav_circle{
+        transform: rotate(45deg);
+    }
+   .nav_link{
+       margin-left: 0.5rem;
+       transition: all 0.2s ease-out;
+       text-decoration: none;
+       color: var(--color-text-grey);
+   }
+   .nav_:hover > .nav_link{
+       cursor: pointer;
+       color:#fff
+   }
 
 </style>
