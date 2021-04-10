09456bfd61146abf243ff96380965cea114e4fd6
WeijieFu <weijiefu92@gmail.com>
2021-04-10 20:15:27 +0200

    update images

--- pages/Appitur/index.vue
+++ pages/Appitur/index.vue
@@ -155,20 +155,32 @@
         />
         <ChapterLeft
             title=""
-            text="Behavioural pattern found among target users. Key quotes : "
+            text="From our talk with Kevin, we came to understand the interest of Appitur's clients. Kevin, who already has a brand Hock International, would like the app to be an addition to the whole family package (books, videos and test questions) he is offering to his students. In the long term, he hopes the Appitur app could somehow inherit their brand identity. He also shared with us some concerns his students have: "
         >
             <List
                 :listItems="[
-                `- can't study much on weekdays while I am working`,
-                `- 100 hours of study is my goal`,
-                `- I like to mark questions I got wrong for further review`,
-                `- I prefer 20 questions at a time`,
-                `- time management is crucial to exam prep`,
-                `- put my phone away when I study on my compute`,
+                `- Provide more explanations (already in database) on all four choices, both right answers and wrong answers.`,
+                `- Add flashcards.`,
+                `- For performance session, it would be helpful if they can be broken down topic by topic.`,
+                `- Sign-up and Log-in is confusing on app.`,
+           
                 ]"
             />
         </ChapterLeft>
 
+        <ChapterLeft
+            title="Usability Testing"
+            text="We conducted usability tests with five participants to test the existing product, understand user behaviour and define success for both the users and the stakeholders."
+        />
+        <ChapterLeft
+            title=""
+            text="The test session is designed to gather general information from the participants and test usability of Appitur apps. In this case, we are testing the HOCK CIA (Certified Internal Auditor) app since all four apps are quite similar."
+        />
+
+         <ImageMain
+            :imageSrc="root+'appitur/05_user_testing.png'"
+            imageAlt="banner"
+        />
        
     </div>
 </template>
