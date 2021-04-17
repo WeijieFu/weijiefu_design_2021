f82bfdf0faa76e6b324ee3520e0a9d17dbeca85f
WeijieFu <weijiefu92@gmail.com>
2021-04-17 22:42:30 +0200

    update dreamatorium

--- pages/Dreamatorium/index.vue
+++ pages/Dreamatorium/index.vue
@@ -3,30 +3,36 @@
         <Mouse :isBlack="true"/>
         <HomeButton/>
 
-        <iframe src="https://elastic-dijkstra-f80045.netlify.app/" title="SWON" ></iframe>
+        <iframe src="https://elastic-dijkstra-f80045.netlify.app/" title="Ducky 3d's 80s animation loop" class="no-pointer" ></iframe>
         <div class="enter">
-            <a href="https://elastic-dijkstra-f80045.netlify.app/" target="_blank">DUCKY3D's 80s Loop &rarr;</a>
+            <a href="https://elastic-dijkstra-f80045.netlify.app/" target="_blank" >DUCKY3D's 80s Loop &rarr;</a>
             <p>Blender, ReactJS, ThreeJS, WebGL</p>
         </div>
      
 
-        <iframe src="https://friendly-panini-d29995.netlify.app/" title="SWON" ></iframe>
+        <iframe src="https://friendly-panini-d29995.netlify.app/" title="Triangle tunnel"  class="no-pointer"></iframe>
         <div class="enter">
             <a href="https://friendly-panini-d29995.netlify.app/" target="_blank">TRIANGLE TUNNEL &rarr;</a>
             <p>Blender, ReactJS, ThreeJS, WebGL</p>
         </div>
 
-        <iframe src="https://tender-leavitt-8d4f37.netlify.app/" title="SWON" ></iframe>
+        <iframe src="https://tender-leavitt-8d4f37.netlify.app/" title="Kiss" class="no-pointer" ></iframe>
         <div class="enter">
             <a href="https://tender-leavitt-8d4f37.netlify.app/" target="_blank">KISS &rarr;</a>
             <p>ThreeJS, WebGL, Raymarching</p>
         </div>
 
 
-        <iframe src="https://musing-leavitt-544406.netlify.app/" title="SWON" ></iframe>
+        <iframe src="https://musing-leavitt-544406.netlify.app/" title="Swirl"  class="no-pointer"></iframe>
         <div class="enter">
             <a href="https://musing-leavitt-544406.netlify.app/" target="_blank">SWIRL &rarr;</a>
             <p>ThreeJS, WebGL, Particles</p>
+        </div>
+
+        <iframe src="https://silly-ptolemy-b99751.netlify.app/" title="Kinectic typography"  ></iframe>
+        <div class="enter">
+            <a href="https://silly-ptolemy-b99751.netlify.app/" target="_blank">KINECTIC TYPOGRAPHY &rarr;</a>
+            <p>Html Canvas, Particles</p>
         </div>
          
     
@@ -62,7 +68,8 @@ iframe {
     height: 45vw;
     margin-left: 10vw;
     border: none;
-    pointer-events: none;
+    /* pointer-events: none; */
+
 }
 .enter{
     font-family: 'Exo 2', sans-serif;
@@ -85,7 +92,9 @@ iframe {
 .enter p{
     color: #ddd;
 }
-
+.no-pointer{
+    pointer-events: none;
+}
  @media only screen and (max-width: 600px) {
         .enter {
               margin-left: 10vw;
